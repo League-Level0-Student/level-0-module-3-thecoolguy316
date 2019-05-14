e7a7da4bf0ee0fd4222d477d5078e255dc4862c0
86742 <86742@0338S-S-R0B6ZHN.sdcs.local>
2019-05-14 14:30:47 -0700

    ShinyObjects, BananaQuiz

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,7 +10,17 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String answer1 = JOptionPane.showInputDialog("Do you like bananas;) *wink wink*");
 		//2. if they say no, 
+		if (answer1.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "YOOOOOUU AAREEE CRAAAAAAAAAAAAAAAAZZZYYYY!!!!!!!!!!!!!!");
+			System.exit(0);
+		}
+		else if (answer1.equalsIgnoreCase("yes")){
+			String hobby = JOptionPane.showInputDialog("Whats your favorite hobby?");
+			JOptionPane.showMessageDialog(null, hobby + "is much better with bananas!");
+			
+		}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -8,18 +8,20 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+String convert = JOptionPane.showInputDialog("How many shiny objects do you want?");
+int shinyobjects = Integer.parseInt(convert);
 		// 3. Play the sound that many times
-
+for (int i=0;i<shinyobjects;i++) {
 		// 1. Call the method below
-
+playMisterZee();
 	}
-
+	}
 	public static void playMisterZee() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(ShinyObjects.class.getResource("shiny-objects.wav"));
