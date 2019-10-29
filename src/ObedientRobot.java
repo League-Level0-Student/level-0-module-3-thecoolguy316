9aea54dbbe22a30f00def3c46170db3ad644ccf9
86742 <86742@0338S-S-R0B6ZHN.sdcs.local>
2019-10-29 14:26:59 -0700

    ObedientRobot

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,14 +1,35 @@
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
-	static Robot Obe = new Robot ();
-public static void main(String[] args) {
-	drawSquare();
-}
-public static void drawSquare() {
-for (int i = 0; i <4;i++) {	
-	Obe.move(50);
-	Obe.turn(90);
-}
-}	
+	static Robot Obe = new Robot();
+
+	public static void main(String[] args) {
+
+	}
+
+	public static void drawSquare() {
+		for (int i = 0; i < 4; i++) {
+			Obe.setSpeed(100);
+			Obe.penDown();
+			Obe.move(100);
+			Obe.turn(90);
+		}
+	}
+	public static void drawTriangle() {
+		for (int i = 0; i <3; i++) {
+			Obe.setSpeed(100);
+			Obe.penDown();
+			Obe.move(100);
+			Obe.turn(120);
+			
+		}
+	}
+	public static void drawCircle() {
+		for (int i = 0; i <72; i++) {
+		Obe.setSpeed(200);
+		Obe.penDown();
+		Obe.move(6);
+		Obe.turn(5);
+	}
+	}		
 }
